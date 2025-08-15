bc7b8c640ca76d23062966c88656ae81613b41be
Yan Carlos <yan.c.oliveira08@gmail.com>
2025-08-15 18:06:37 -0300

    Arquivo que conecta ao banco via PDO

--- ConexaoPadaria/PHP/conexao.php
+++ ConexaoPadaria/PHP/conexao.php
@@ -4,8 +4,8 @@ $user = "root";
 $password = "";
 
 try{
-    $con = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+    $con = new PDO($dsn, $user, $password, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 }catch(PDOException $e){
-    echo "ERRO PAE: ".$e->getMessage();
+    echo "Erro ao conectar com o Banco: ".$e->getMessage();
 }
 ?>
