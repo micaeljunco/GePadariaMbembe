a7a4f4ce9ff72383bdc91a518fe278e897896dbf
Micael Junco <micael.j.junco@gmail.com>
2025-08-22 17:05:08 -0300

    função mais robusta
    adicionei tipos de retorno e try...catch na função que pode retornar todos os fornecedores ou um erro.

--- html/model_select_fornecedores.php
+++ html/model_select_fornecedores.php
@@ -1,10 +1,19 @@
 <?php
 require_once "../PHP/conexao.php";
 
-function consulta_fornecedores() {
+function consulta_fornecedores(): array|string
+{
     global $con;
-    $sql= "SELECT * FROM fornecedores";
-    $stmt= $con->prepare($sql);
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $sql = "SELECT * FROM `fornecedores`";
+    $stmt = $con->prepare($sql);
+    try {
+        if ($stmt->execute()) {
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        }
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+
+    return [];
 }
