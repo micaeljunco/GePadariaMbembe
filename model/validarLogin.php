d4e3e555829ee6abe3f75e6633e03032a03ad752
Carlos-Yan0 <yan.c.oliveira08@gmail.com>
2025-08-23 18:16:51 -0300

    Chamada da autenticação do login
    Aqui chamamos a função auth, que vai verificar o login

---
+++ model/validarLogin.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ ."/usuario/email.php";
+require_once __DIR__ ."/usuario/senha.php";
+require_once __DIR__ ."/auth.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    try{
+        $email = new Email($_POST["email"]);
+        $senha = new Senha($_POST["senha"]);
+
+        auth::login($email, $senha);
+    }catch(Exception $e){
+        echo "<script>alert('Erro: " . addslashes($e->getMessage()) . "');window.location.href='../view/index.php'</script>";
+        exit();
+    }
+}else{
+    header("Location: ../view/index.php");
+}
+
+
+?>
