14e445642876cdcd5bfcc3e1511a85e94c69038e
Carlos-Yan0 <yan.c.oliveira08@gmail.com>
2025-08-23 18:21:10 -0300

    Class de Senha
    A class de senha verifica se o tamanho da senha é maior que 8 e alem disso possui os metodos de verificarSenha(para login e ações importantes) e tambem gera a senha em Hash

---
+++ model/usuario/senha.php
@@ -0,0 +1,28 @@
+<?php
+
+class Senha{
+    private string $senhaDigitada;
+
+    public function __construct(string $senha){
+        if(strlen($senha) < 8){
+            throw new InvalidArgumentException("A senha deve ter 8 ou mais caracteres!");
+        }
+        $this->senhaDigitada = $senha;
+        
+    }
+
+    public function gerarHash(): string{
+        return password_hash($this->senhaDigitada, PASSWORD_DEFAULT);
+    }
+
+    public function verificarSenha(string $senhaHash): bool{
+        return password_verify($this->senhaDigitada, $senhaHash);
+    }
+
+    public function __toString(): string{
+        return $this->senhaDigitada;
+    }
+
+}
+
+?>
