efe2a47d0088f213c51d1a8a8b4fe4f26a67ddf9
Carlos-Yan0 <yan.c.oliveira08@gmail.com>
2025-08-23 18:16:24 -0300

    Autenticação de login
    Aqui verificamos o login do usuario

---
+++ model/auth.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require_once __DIR__ ."/../conexao.php";
+require_once __DIR__ ."/usuario/email.php";
+require_once __DIR__ ."/usuario/senha.php";
+
+class auth{
+
+    public static function login(Email $email, Senha $senha): bool{
+        global $con;
+
+        $sql = "SELECT * FROM usuarios WHERE email = :email";
+        $stmt = $con->prepare($sql);
+        $email = (string) $email;
+        $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+        $stmt->execute();
+        $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($usuario && $senha->verificarSenha($usuario["senha"])){
+            $_SESSION["nome"] = $usuario['nome_usuario'];
+            $_SESSION["id_usuario"] = $usuario['id_usuario'];
+            $_SESSION["id_cargo"] = $usuario['id_cargo'];
+
+            echo "<script>alert('Login realizado com Sucesso!');window.location.href='../view/home.php'</script>";
+            return true;
+        }else{
+            echo "<script>alert('E-mail ou senha Incorretos, tente novamente!');window.location.href='../view/index.php'</script>";
+            return false;
+        }
+        
+
+    }
+}
+?>
