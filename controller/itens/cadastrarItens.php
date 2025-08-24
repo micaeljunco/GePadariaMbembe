dbcba647f59992eb3f6cd5dfb6936cf0d2dab67c
Micael Junco <micael.j.junco@gmail.com>
2025-08-24 16:43:59 -0300

    efetua o cadastro dos itens

---
+++ controller/itens/cadastrarItens.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . "/controllerItens.php";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    cadastrar_item();
+} else {
+    header("Location: ../../view/itens.php");
+}
