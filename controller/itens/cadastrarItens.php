567ad732b642d8b4a21e95631cbe5d3e6247603c
Micael Junco <micael.j.junco@gmail.com>
2025-08-26 17:58:32 -0300

    correção: !... > !==
    erro bobo

--- controller/itens/cadastrarItens.php
+++ controller/itens/cadastrarItens.php
@@ -1,9 +1,8 @@
 <?php
 require_once __DIR__ . "/controllerItens.php";
 
-if (!$_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     header("Location: ../../view/itens.php");
 }
 
 cadastrar_item();
-?>
