7ae4da9194cfc356bdbd3c8206086683f6c49f8f
Micael Junco <micael.j.junco@gmail.com>
2025-08-26 17:58:09 -0300

    funções: deletar_itens E editar_item
    editar_item(): processa a edição de dados de qualquer item
    deletar_itens: deleta qualquer item da tabela.

--- controller/itens/controllerItens.php
+++ controller/itens/controllerItens.php
@@ -73,3 +73,84 @@ function cadastrar_item(): void
         exit();
     }
 }
+
+function deletar_item(int $id_item): void
+{
+    global $con;
+
+    $sql = "DELETE FROM itens WHERE id_item = :id_item";
+    $stmt = $con->prepare($sql);
+    $stmt->bindParam(":id_item", $id_item, PDO::PARAM_INT);
+
+    try {
+        if ($stmt->execute()) {
+            echo "<script>alert('Item deletado com sucesso.');window.location.href = '../../view/itens.php'</script>";
+        }
+    } catch (PDOException $e) {
+        echo "<script>alert('Não foi possível realizar a operação. Detalhes: " .
+            $e->getMessage() .
+            "');window.location.href = '../../view/itens.php'</script>";
+    }
+}
+
+function editar_item(): void
+{
+    global $con;
+    try {
+        $id_item = (int) $_POST["id_item"];
+
+        $sql = "SELECT * FROM itens WHERE id_item = :id_item";
+        $stmt = $con->prepare($sql);
+        $stmt->bindParam(":id_item", $id_item, PDO::PARAM_INT);
+        $stmt->execute();
+        $infoItens = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $nome_item = new Nome($_POST["nomeItem"]);
+        $quant_min = (int) $_POST["quantMin"];
+        $quant = (int) $_POST["quant"];
+        $categoria = (string) $_POST["categoria"];
+        $validade = (string) $_POST["validade"];
+        $id_fornecedor = (int) $_POST["idFornecedor"];
+        $val_unitario = (float) $_POST["valUni"];
+
+        $item = new Item(
+            0,
+            $nome_item,
+            $quant_min,
+            $quant,
+            $categoria,
+            $validade,
+            $id_fornecedor,
+            $val_unitario,
+        );
+
+        $sql = "UPDATE itens SET nome_item = :nome_item, quant_min = :quant_min, quant = :quant, categoria = :categoria,
+                validade = :validade, id_fornecedor = :id_fornecedor, val_unitario = :val_unitario WHERE id_item = :id_item";
+
+        $stmt = $con->prepare($sql);
+        $stmt->bindValue(":nome_item", $item->getNomeItem(), PDO::PARAM_STR);
+        $stmt->bindValue(":quant_min", $item->getQuantMin(), PDO::PARAM_INT);
+        $stmt->bindValue(":quant", $item->getQuant(), PDO::PARAM_INT);
+        $stmt->bindValue(":categoria", $item->getCategoria(), PDO::PARAM_STR);
+        $stmt->bindValue(":validade", $item->getValidade(), PDO::PARAM_STR);
+        $stmt->bindValue(
+            ":id_fornecedor",
+            $item->getIdFornecedor(),
+            PDO::PARAM_INT,
+        );
+        $stmt->bindValue(":val_unitario", $item->getValUni(), PDO::PARAM_STR);
+        $stmt->bindParam(":id_item", $id_item, PDO::PARAM_INT);
+
+        if (!$stmt->execute()) {
+            echo "<script>alert('Não foi possivel atualizar o item, Tente novamente!');window.location.href='../../view/itens.php'</script>";
+            exit();
+        }
+        echo "<script>alert('Item atualizado com sucesso!');window.location.href='../../view/itens.php'</script>";
+        exit();
+    } catch (InvalidArgumentException $e) {
+        echo "<script>alert('" .
+            addslashes($e->getMessage()) .
+            "');window.location.href='../view/itens.php'</script>";
+        exit();
+    }
+}
