ea6e101206e287ed3b539e3e4f43e7131ac67710
Micael Junco <micael.j.junco@gmail.com>
2025-08-30 17:50:16 -0300

    adição: logout.php
    realiza o logout do usuário e redireciona para a página de login

---
+++ view/logout.php
@@ -0,0 +1,26 @@
+<?php
+session_start(); // sempre iniciar a sessão antes
+
+// Limpar todas as variáveis da sessão
+$_SESSION = [];
+
+// Apagar o cookie de sessão (importante!)
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        "",
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"],
+    );
+}
+
+// Destruir a sessão
+session_destroy();
+
+// Redirecionar
+header("Location: ./");
+exit();
