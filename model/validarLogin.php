87a3a63dd8f01dacd1bf4a323cb884b010b13f18
Micael Junco <micael.j.junco@gmail.com>
2025-08-31 15:39:18 -0300

    Update validarLogin.php

--- model/validarLogin.php
+++ model/validarLogin.php
@@ -1,21 +1,20 @@
 <?php
-require_once __DIR__ ."/usuario/email.php";
-require_once __DIR__ ."/usuario/senha.php";
-require_once __DIR__ ."/auth.php";
+require_once __DIR__ . "/usuario/email.php";
+require_once __DIR__ . "/usuario/senha.php";
+require_once __DIR__ . "/auth.php";
 
-if(!$_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     header("Location: ../view/index.php");
 }
 
-try{
-        $email = new Email($_POST["email"]);
-        $senha = new Senha($_POST["senha"]);
+try {
+    $email = new Email($_POST["email"]);
+    $senha = new Senha($_POST["senha"]);
 
-        auth::login($email, $senha);
-    }catch(Exception $e){
-        echo "<script>alert('Erro: " . addslashes($e->getMessage()) . "');window.location.href='../view/index.php'</script>";
-        exit();
-    }
-
-
-?>
+    auth::login($email, $senha);
+} catch (Exception $e) {
+    echo "<script>alert('Erro: " .
+        addslashes($e->getMessage()) .
+        "');window.location.href='../view/index.php'</script>";
+    exit();
+}
