fc3507991e35420bd37af75858c92c898716fe06
Lucas-Magalhaes-oficial <lucas.magalhaes.lms@gmail.com>
2025-09-02 16:53:41 -0300

    update teste cadastro dos itens
    agora funcional

--- testes/teste_cadastro_itens.py
+++ testes/teste_cadastro_itens.py
@@ -1,10 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
 driver.maximize_window()
+
+# === LOGIN ===
+driver.get("http://localhost:8080/GePadariaMbembe/view/index.php")
+
+# Preenche e-mail e senha
+time.sleep(2)
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "email"))).send_keys("lucas.magalhaes.lms@gmail.com")
+time.sleep(2)
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "senha"))).send_keys("12345678")
+time.sleep(2)
+
+# Clica no botão "Entrar"
+driver.find_element(By.ID, "btn-entrar").click()
+time.sleep(1)
+
+# Espera o alerta aparecer e clica em "OK"
+WebDriverWait(driver, 10).until(EC.alert_is_present())
+alert = driver.switch_to.alert
+alert.accept()
 
 # Abre a página de itens
 driver.get("http://localhost:8080/GePadariaMbembe/view/itens.php")
@@ -38,7 +59,7 @@ time.sleep(1)
 
 # Categoria (select)
 categoria_select = Select(driver.find_element(By.NAME, "categoria"))
-categoria_select.select_by_visible_text("insumo")
+categoria_select.select_by_visible_text("Insumo")
 time.sleep(1)
 
 # Unidade de Medida (select)
