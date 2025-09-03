bab232da7533385522b40783ed750af3d0946ed9
Micael Junco <micael.j.junco@gmail.com>
2025-09-03 15:47:43 -0300

    função: finalizar_compra()
    essa função chama outras funções que fazem registros em três tabelas do banco: vendas, metodo_pag e venda_itens

--- controller/pdv/controllerPdv.php
+++ controller/pdv/controllerPdv.php
@@ -1,9 +1,12 @@
 <?php
 require_once __DIR__ . "/../../model/itens/classItens.php";
+require_once __DIR__ . "/../../model/vendas/vendas.php";
+require_once __DIR__ . "/../../model/vendas/vendas_itens.php";
+require_once __DIR__ . "/../../model/vendas/metodo_pag.php";
 require_once __DIR__ . "/../../conexao.php";
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+
+session_start();
+
 removerItem();
 recalcular_total();
 // adicionar_item(); // ← ESSENCIAL
@@ -50,7 +53,8 @@ function removerItem()
     }
 }
 
-function recalcular_total() {
+function recalcular_total()
+{
     $total = 0.0;
     if (isset($_SESSION['itens'])) {
         foreach ($_SESSION['itens'] as $item) {
@@ -69,7 +73,8 @@ function atualizar_total($val_uni, $quant): void
 }
 
 
-function processa_metodos(): void {
+function processa_metodos(): void
+{
     // Detectar método
     $metodo = $_POST['metodo'] ?? null;
 
@@ -99,7 +104,8 @@ function processa_metodos(): void {
 
 }
 
-function atualizar_subtotal($valor) {
+function atualizar_subtotal($valor)
+{
     $subtotal = (float) ($_SESSION['subtotal'] ?? $_SESSION['total']);
     $subtotal -= $valor;
 
@@ -116,16 +122,149 @@ function atualizar_subtotal($valor) {
 }
 
 
-function finalizar_compra(): void {
-    limpar_venda();
+function finalizar_compra(): void
+{
+    try {
+        $id_venda = cadastrar_venda(); // cadastra venda e retorna id
+
+        venda_itens($id_venda);        // cadastra itens relacionados
+
+        insert_metodo_pag($id_venda);
+
+        
+        echo "
+        <script>
+        alert('Venda registrada com sucesso.');
+        window.location.href = '../../view/pdv.php';
+        </script>";
+        limpar_venda(false);
+
+    } catch (Exception $e) {
+        echo "
+        <script>
+        alert('Erro ao registrar venda. Detalhes: " . addslashes($e->getMessage()) . "');
+        window.location.href = '../../view/pdv.php';
+        </script>";
+        limpar_venda(false);
+    }
 }
 
-function limpar_venda() {
+function cadastrar_venda(): int
+{
+    global $con;
+
+    $id_usuario = $_SESSION["id_usuario"];
+    $data_hora = new DateTime();
+    $valor_total = $_SESSION["total"];
+
+    $venda = new Vendas(
+        0,
+        $id_usuario,
+        NULL,
+        $data_hora,
+        $valor_total
+    );
+
+    $sql = "INSERT INTO `vendas`(`id_usuario`, `id_comanda`, `valor_total`, `data_hora`) 
+            VALUES (:id_usuario, :id_comanda, :valor_total, :data_hora)";
+    $stmt = $con->prepare($sql);
+
+    $stmt->bindValue(":id_usuario", $venda->getIdUsuario(), PDO::PARAM_INT);
+
+    $id_comanda = $venda->getIdComanda();
+    if ($id_comanda === null) {
+        $stmt->bindValue(":id_comanda", null, PDO::PARAM_NULL);
+    } else {
+        $stmt->bindValue(":id_comanda", $id_comanda, PDO::PARAM_INT);
+    }
+
+    $stmt->bindValue(":valor_total", $venda->getValorTotal(), PDO::PARAM_INT);
+    $stmt->bindValue(":data_hora", $venda->getDataHora()->format('Y-m-d H:i:s'), PDO::PARAM_STR);
+
+    if (!$stmt->execute()) {
+        throw new Exception("Falha ao inserir venda.");
+    }
+
+    return (int) $con->lastInsertId();
+}
+
+function venda_itens(int $id_venda): void
+{
+    global $con;
+
+    $itens = $_SESSION["itens"] ?? [];
+
+    if (empty($itens)) {
+        return; // sem itens para cadastrar
+    }
+
+    foreach ($itens as $item) {
+        $venda_itens = new VendaItens(
+            0,
+            $id_venda,
+            $item["id_item"],
+            $item["quantidade"]
+        );
+
+        $sql = "INSERT INTO `vendas_itens`(`id_venda`, `id_item`, `quantidade`) 
+                VALUES (:id_venda, :id_item, :quantidade)";
+
+        $stmt = $con->prepare($sql);
+
+        $stmt->bindValue(":id_venda", $venda_itens->getIdVenda(), PDO::PARAM_INT);
+        $stmt->bindValue(":id_item", $venda_itens->getIdItem(), PDO::PARAM_INT);
+        $stmt->bindValue(":quantidade", $venda_itens->getQuantidade(), PDO::PARAM_INT);
+
+        if (!$stmt->execute()) {
+            throw new Exception("Falha ao inserir item da venda.");
+        }
+    }
+}
+
+function insert_metodo_pag(int $id_venda) {
+    global $con;
+
+    $metodos_pagamento = $_SESSION["metodos_pagamento"] ?? [];
+
+    if (empty($metodos_pagamento)) {
+        return; // sem nada para cadastrar
+    }
+
+    foreach ($metodos_pagamento as $metodo) {
+        if (!empty($p["cartao"])):
+            $metodo["metodo"] .= ["cartao"];
+        endif;
+        $class_metPag = new MetodoPagamento(
+            0,
+            $id_venda,
+            $metodo["metodo"],
+            $metodo["valor"]
+        );
+
+        $sql = "INSERT INTO `metodos_pag`(`id_venda`, `metodo`, `valor_pago`) 
+                VALUES (:id_venda, :metodo, :valor_pago)";
+
+        $stmt = $con->prepare($sql);
+
+        $stmt->bindValue(":id_venda", $class_metPag->getIdVenda(), PDO::PARAM_INT);
+        $stmt->bindValue(":metodo", $class_metPag->getMetodo(), PDO::PARAM_STR);
+        $stmt->bindValue(":valor_pago", $class_metPag->getValorPago(), PDO::PARAM_STR);
+
+        if (!$stmt->execute()) {
+            throw new Exception("Falha ao inserir método de pagamento da venda.");
+        }
+    }
+}
+
+function limpar_venda($redirecionar)
+{
     unset($_SESSION["itens"]);
     unset($_SESSION["total"]);
     unset($_SESSION["subtotal"]);
     unset($_SESSION["troco"]);
     unset($_SESSION["metodos_pagamento"]);
-    header("Location: ../../view/pdv.php");
+    if ($redirecionar) {
+        header("Location: ../../view/pdv.php");
+    }
     exit;
 }
