02d979d57583904d899214275d6f74bbaaeb125e
Yan Carlos <yan.c.oliveira08@gmail.com>
2025-09-04 15:47:17 -0300

    update 2 banco
    arrumei o use do codigo sql

--- SA_BD/sa_padaria_mokele.sql
+++ SA_BD/sa_padaria_mokele.sql
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 -- Banco de dados: `sa_padaria_mokele`
 --
 create database sa_padaria_mokele;
-use database sa_padaria_mokele;
+use sa_padaria_mokele;
 
 -- --------------------------------------------------------
 
